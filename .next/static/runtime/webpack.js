d3a642f2c07d63ec7a22c6f8dba41f01fcd247d6
Marius Grondahl <marius@grondahldesign.no>
2020-06-30 18:08:01 +0200

    minor fix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d3612d225b7aeb71431";
+/******/ 	var hotCurrentHash = "7c576b8dbeee9016a800";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
