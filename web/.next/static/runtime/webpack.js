d5dd19dd176623bf01caef64a3cd3346cb3098ca
Marius Grondahl <marius@grondahldesign.no>
2020-07-02 15:56:02 +0200

    changed basepaths

--- web/.next/static/runtime/webpack.js
+++ web/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "75a9aed22c79c65a1708";
+/******/ 	var hotCurrentHash = "3cb7db1286b7fdc08aed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
